74d612b5a6e8f572d46d0bec32f3da5e707bb8c5
Amy <127365502+LdaAmy@users.noreply.github.com>
2025-09-11 10:19:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+new Vue({
+  el: '#app',
+  data: {
+    ad: true,
+    ad2: false,
+    ad3: false,
+    musicas: [
+      { nome: 'Musica 1' },
+      { nome: 'Musica 2' },
+      { nome: 'Musica 3' },
+      { nome: 'Musica 4' },
+      { nome: 'Musica 5' },
+      { nome: 'Musica 6' },
+      { nome: 'Musica 7' },
+      { nome: 'Musica 8' },
+      { nome: 'Musica 9' },
+      { nome: 'Musica 10' },
+    ]
+  },
+  methods: {
+    close(){
+      this.ad = false
+      this.ad2 = true
+      this.ad3 = true
+    }
+  }
+});
